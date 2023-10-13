791fa9badf567b8174ce45cc273761f3fd8ed796
Duinrahaic <gman2306@gmail.com>
2023-10-13 10:46:29 -0500

    Changed from CSS to SCSS for cleanliness

---
+++ dOSC/Components/Form/FormInput.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace dOSC.Components.Form
+{
+    public partial class FormInput
+    {
+        [Parameter]
+        public FormInputType InputType { get; set; } = FormInputType.None;
+
+        [Parameter]
+        public dynamic Value { get; set; }
+
+        [Parameter]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Parameter] 
+        public string Label { get; set; } = string.Empty;
+
+        [Parameter]
+        public RenderFragment? TopContent { get; set; }
+
+        [Parameter] 
+        public RenderFragment? BottomContent { get; set; }
+
+        public enum FormInputType
+        {
+            Text,
+            Number,
+            Email,
+            Password,
+            Date,
+            Time,
+            DateTime,
+            Month,
+            Week,
+            Search,
+            Tel,
+            Url,
+            Color,
+            Range,
+            File,
+            Checkbox,
+            Radio,
+            Select,
+            TextArea,
+            Hidden,
+            Submit,
+            Reset,
+            Button,
+            Image,
+            None,
+        }
+    }
+}
---
+++ dOSC/Components/UI/AppDetailItem.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace dOSC.Components.UI
+{
+    public partial class AppDetailItem
+    {
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
