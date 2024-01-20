d223b796dfe42da8d161c4f42488cb79447e1719
Duinrahaic <gman2306@gmail.com>
2024-01-19 19:39:18 -0600

    Variables now update on link connection

--- dOSCEngine/Engine/Nodes/Variables/LogicNode.cs
+++ dOSCEngine/Engine/Nodes/Variables/LogicNode.cs
@@ -10,16 +10,18 @@ namespace dOSCEngine.Engine.Nodes.Variables
     {
         public LogicNode(Guid? guid = null, ConcurrentDictionary<EntityPropertyEnum, dynamic>? properties = null, Point? position = null) : base(guid, position, properties)
         {
-            AddPort(new LogicPort(PortGuids.Port_1, this, false, name: "Output"));
+            Port = new LogicPort(PortGuids.Port_1, this, false, name: "Output");
+            AddPort(Port);
             Properties.TryInitializeProperty(EntityPropertyEnum.ConstantValue, false);
             Value = Properties.GetProperty<dynamic>(EntityPropertyEnum.ConstantValue);
             VisualIndicator = Value.ToString();
+            Port.OnPortLinksChanged += SendValue;
         }
-
+        
         public override string Name => "Logic Variable";
         public override string Category => NodeCategoryType.Logic;
         public override string Icon => "icon-binary";
-        
+        private LogicPort Port { get; set; }
         public override void PropertyNotifyEvent(EntityPropertyEnum property, dynamic? value)
         {
             if (property == EntityPropertyEnum.ConstantValue)
@@ -27,6 +29,19 @@ namespace dOSCEngine.Engine.Nodes.Variables
                 SetValue(value,true);
                 VisualIndicator = Value.ToString();
             }
+        }
+        private void SendValue(BasePort port)
+        {
+            if (Port.HasValidLinks())
+            {
+                SetValue(Value,true);
+            }
+        }
+
+        public override void OnDispose()
+        {
+            Port.OnPortLinksChanged -= SendValue;
+            base.OnDispose();
         }
     }
 }
--- dOSCEngine/Engine/Nodes/Variables/NumericNode.cs
+++ dOSCEngine/Engine/Nodes/Variables/NumericNode.cs
@@ -8,14 +8,17 @@ public class NumericNode : BaseNode
 {
     public NumericNode(Guid? guid = null, ConcurrentDictionary<EntityPropertyEnum, dynamic>? properties = null, Point? position = null) : base(guid, position, properties)
     {
-        AddPort(new NumericPort(PortGuids.Port_1, this, false, name: "Output"));
+        Port = new NumericPort(PortGuids.Port_1, this, false, name: "Output");
+        AddPort(Port);
         Properties.TryInitializeProperty(EntityPropertyEnum.ConstantValue, 0.0);
         Value = Properties.GetProperty<dynamic>(EntityPropertyEnum.ConstantValue);
         VisualIndicator = Value.ToString("G5");
+        Port.OnPortLinksChanged += SendValue;
     }
     public override string Name => "Numeric Variable";
     public override string Category => NodeCategoryType.Math;
     public override string TextIcon => "#";
+    private NumericPort Port { get; set; }
 
     public override void PropertyNotifyEvent(EntityPropertyEnum property, dynamic? value)
     {
@@ -24,6 +27,20 @@ public class NumericNode : BaseNode
             SetValue(value,true);
             VisualIndicator = Value.ToString("G5");
         }
+    }
+    
+    private void SendValue(BasePort port)
+    {
+        if (Port.HasValidLinks())
+        {
+            SetValue(Value,true);
+        }
+    }
+
+    public override void OnDispose()
+    {
+        Port.OnPortLinksChanged -= SendValue;
+        base.OnDispose();
     }
 }
 
